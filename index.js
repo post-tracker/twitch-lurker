b9104c2256024f33aff58ff4f9afd6a4c7524e08
Oskar Risberg <oskar.risberg@stendahls.se>
2018-01-05 21:10:07 +0100

    Initial commit

---
+++ index.js
@@ -0,0 +1,10 @@
+const streams = [
+    'kotton',
+    'klean_uppguy',
+];
+const devs = [
+    'EscapeFromTarkov',
+];
+
+// Connect to the chat for all streams. Wait for replys from all devs.
+// Optionally only connect when they are live?
