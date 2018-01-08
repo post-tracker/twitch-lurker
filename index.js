df142bbca64ef224344703d8b2dcbdca2d832d38
injectJon <injectJon@gmail.com>
2018-01-07 18:41:13 -0600

    Get streams via kraken API

--- index.js
+++ index.js
@@ -1,13 +1,8 @@
-require('dotenv').config();
-const tmi = require('tmi.js');
+require( 'dotenv' ).config();
+const tmi = require( 'tmi.js' );
 const got = require( 'got' );
 
-// TODO: [injectJon]
-// Fetch live EFT streams via twitch dev api, connect via IRC to top 20(?)
-// Context for dev messages:
-//     Option 1: Keep a constant 5min message history to grab context message (more context matches)
-//     Option 2: Keep all messages (within 5min) that mention a dev (performant)
-
+const liveStreams = [];
 const devAccounts = [];
 const posts = [];
 let context = [];
@@ -16,8 +11,7 @@ let context = [];
 setInterval( () => {
     if ( context.length === 0 ) return;
 
-    const now = Date.now()
-    const timeSinceMessage = Date.now() - context[ context.length - 1 ].timestamp
+    const timeSinceMessage = Date.now() - context[ context.length - 1 ].timestamp;
 
     if ( timeSinceMessage >= 300000 ) {
         context.pop();
@@ -36,18 +30,54 @@ const apiRequest = function apiRequest( path ) {
         } );
 };
 
-const getStreams = async function getStreams(){
-    console.log( '<info> Getting streams from API...' );
+const twitchApiRequest = function twitchApiRequest( path ) {
+    return got( `https://api.twitch.tv/kraken${ path }`, {
+        headers: {
+            "Accept": "application/vnd.twitchtv.v5+json",
+            "Client-ID": process.env.TWITCH_CLIENTID,
+        },
+    } )
+    .then( response => {
+        return JSON.parse( response.body );
+    } );
+}
+
+const getGames = async function getGames() {
+    console.log( '<info> Fetching games from API...' );
     const gamesResponse = await apiRequest( '/games' );
 
+    const games = [];
+
     for ( let i = 0; i < gamesResponse.data.length; i = i + 1 ) {
-        if ( gamesResponse.data[ i ].identifier === 'escape-from-tarkov' ) {
-            return gamesResponse.data[ i ].config.sources.Twitch.allowedSections.map( ( streamName ) => {
-                return `#${ streamName }`;
-            } );
+        if ( gamesResponse.data[ i ].config.sources && gamesResponse.data[ i ].config.sources.Twitch ) {
+            games.push( gamesResponse.data[ i ].config.sources.Twitch.name );
         }
     }
-};
+
+    return games;
+}
+
+const getStreams = async function getStreams( games ) {
+    console.log( '<info> Getting streams from kraken API' );
+
+    for ( let i = 0; i < games.length; i++ ) {
+        let game = games[ i ];
+        const split = game.split( ' ' )
+        const needsEncoding = ( split.length >= 2 )
+
+        if ( needsEncoding ) {
+            game = split.join( '+' );
+        }
+
+        const apiPath = `/search/streams?query=${ game }&limit=25`;
+        const streamsResponse = await twitchApiRequest( apiPath );
+
+        for ( let j = 0; j < streamsResponse.streams.length; j++ ) {
+            const stream = `#${ streamsResponse.streams[ j ].channel.name }`;
+            liveStreams.push( stream );
+        }
+    }
+}
 
 const getDevelopers = async function getDevelopers(){
     console.log( '<info> Getting developers from API...' );
@@ -132,10 +162,10 @@ function messageHandler( data ) {
 
 }
 
-// Connect to the chat for all streams. Wait for replys from all devs.
-// Optionally only connect when they are live?
 
 function twitchIrc( channels, devs ) {
+    console.log( '<info> Listening for dev activity...' );
+
     // Twitch IRC client config options
     /* Docs: https://docs.tmijs.org/v1.2.1/Configuration.html */
     const config = {
@@ -168,13 +198,13 @@ function twitchIrc( channels, devs ) {
 
 let streams;
 
-getStreams()
-    .then( ( allStreams ) => {
-        streams = allStreams;
-
+getGames()
+    .then( games => {
+        return getStreams( games );
+    } )
+    .then( () => {
         return getDevelopers();
     } )
-    .then( ( allDevs ) => {
-        console.log( '<info> Listening for dev activity...' );
-        twitchIrc( streams, allDevs );
+    .then( allDevs => {
+        twitchIrc( liveStreams, allDevs );
     } );
